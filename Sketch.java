a70df11566c400f71dd95fc58fcf20f864f17720
ChrisTarla <christine.tarla26@ycdsbk12.ca>
2023-12-16 19:40:12 -0500

    worked on task

--- Sketch.java
+++ Sketch.java
@@ -1,11 +1,7 @@
 /** Instructions 
- * A sketch that simulates snow falling; down arrow on keyboard makes snow falls faster, up arrow makes snow fall slower 
- * Add a blue player circle that is controlled by WASD keys; add three squares at the top right of the screen for player lives; player loses a life 
- * everytime it collides with snowflake; game ends when all lives are lost and screen clears to white 
- * Add mouse control so players can click on snowflakes and make them disappear while using WASD keys with other hand (may need bigger snowflakes) 
+ * Player loses a life everytime it collides with snowflake  
+ * Add mouse control so players can click on snowflakes and make them disappear while using WASD keys with other hand 
  * 
- * TIP: have a related array of x values for snowflakes and initialize them to be at randomly generated locations (just like the y values) 
- * instead of setting your x-values of the snowballs to be a function of the randomly generated y 
  * TIP: introduce an additional "ballHideStatus" related array to store the state of each snowball; elements will be defaulted to false, and when a 
  * snowball is clicked on, the corresponding ballHideStatus will become true and the snowball will no longer be drawn to the screen 
  */
@@ -13,14 +9,22 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	// Variables 
-  boolean blnUp;
-  boolean blnLeft;
-  boolean blnDown;
-  boolean blnRight;
+	// Player Variables 
+  boolean blnUp = false;
+  boolean blnLeft = false;
+  boolean blnDown = false;
+  boolean blnRight = false;
   int intPlayerX = 200;
   int intPlayerY = 375;
-
+  // Snow Variables 
+  int intSnowX;
+  float[] fltSnowY = new float[15];
+  int intSnowfall = 2;
+  // Player Lives Variables 
+  boolean blnLife1 = true;
+  boolean blnLife2 = true;
+  boolean blnLife3 = true;
+  
   // Called once at the beginning of execution, put your size all in this method
   public void settings() {
 	// put your size call here
@@ -29,19 +33,71 @@ public class Sketch extends PApplet {
 
   // Called once at the beginning of execution.  Add initial set up values here i.e background, stroke, fill etc. 
   public void setup() {
-    background(210, 255, 173);
+    background(205, 230, 255);
+    for (int i = 0; i < fltSnowY.length; i++){
+      fltSnowY[i] = random(height);
+    }
   }
 
   // Called repeatedly, anything drawn to the screen goes here
   public void draw() {
-	  while (blnUp == true){
-      if (intPlayerY > 25){
-        intPlayerY -= 1;
+    if (blnLife3 == true){
+      // Reset background for animation effect 
+      background(205, 230, 255);
+      
+      // Draw Falling Snow 
+      stroke(255);
+      fill(255);
+      for (int i = 0; i < fltSnowY.length; i++){
+        intSnowX = width * i / fltSnowY.length;
+        ellipse(intSnowX, fltSnowY[i], 25, 25);
+        fltSnowY[i] += intSnowfall;
+    
+        if (fltSnowY[i] > height) {
+          fltSnowY[i] = 0;
+        }
       }
+        
+      // Draw player and movements  
+      playerMovement();
+      fill(130, 195, 250);
+      ellipse(intPlayerX, intPlayerY, 50, 50);
+
+      // Player Lives 
+      stroke(255,70, 70);
+      fill(255, 70, 70);
+      if (blnLife1 == true){
+        rect(355, 5, 10, 10);
+      }
+      if (blnLife2 == true){
+        rect(370, 5, 10, 10);
+      }
+      if (blnLife3 == true){
+        rect(385, 5, 10, 10);
+      }
+    } else {
+      background(255);
     }
-    ellipse(intPlayerX, intPlayerY, 50, 50);
   }
 
+  /**
+   * A program that changes the x and y value of the provided variables based on the conditions of the user keyboard input 
+   */
+  public void playerMovement(){
+    if (blnUp == true && intPlayerY >= 25){
+      intPlayerY -= 3;
+    } else if (blnDown == true && intPlayerY <= 375){
+      intPlayerY += 3;
+    } else if (blnLeft == true && intPlayerX >= 25){
+      intPlayerX -= 3;
+    } else if (blnRight == true && intPlayerX <= 375){
+      intPlayerX += 3;
+    }
+  }
+
+  /**
+   * A program that reads the user keyboard input and changes values according to the pressed keys 
+   */
   public void keyPressed(){
     if (key == 'W' || key == 'w'){
       blnUp = true;
@@ -51,10 +107,19 @@ public class Sketch extends PApplet {
       blnDown = true;
     } else if (key == 'D' || key == 'd'){
       blnRight = true;
+    } else if (key == CODED){
+      if (keyCode == UP){
+        intSnowfall = 1;
+      } else if (keyCode == DOWN){
+        intSnowfall = 4;
+      }
     }
   }
 
-  /*public void keyReleased(){
+  /**
+   * A program that stops the user keyaord input and resets values according to the pressed keys 
+   */
+  public void keyReleased(){
     if (key == 'W' || key == 'w'){
       blnUp = false;
     } else if (key == 'A' || key == 'a'){
@@ -63,6 +128,31 @@ public class Sketch extends PApplet {
       blnDown = false;
     } else if (key == 'D' || key == 'd'){
       blnRight = false;
+    } else if (key == CODED){
+      if (keyCode == UP){
+        intSnowfall = 2;
+      } else if (keyCode == DOWN){
+        intSnowfall = 2;
+      }
     }
-  }*/
+  }
+
+  /**
+   * A program that detects if the player circle collides with the snow, and deducts a life accordingly 
+   */
+  public void playerCollision(){
+    /* check if intPlayerX is between these 2 values of each snow circle, and if intPlayerY is between these 2 values of each snow circle
+       if yes, check which lives are still true, and make the leftmost one false 
+       if all lives are false, make background(255) and stop everything 
+    */
+  }
+  
+  /**
+   * A program that detects if the player clicks on a snow circle, and hides the respective circle 
+   */
+  public void mouseClicked(){
+    /* check if MouseX is between these 2 values of each snow circle, and if MouseY is between these 2 values of each snow circle 
+       if yes, make that snow circle disappear by making a hide status variable true 
+    */
+  }
 }
